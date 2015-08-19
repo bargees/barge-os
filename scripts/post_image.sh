0ef60d8054e606935cdaa15da9cfe6ceb155512c
A.I <ailis@paw.zone>
2015-08-18 22:28:06 -0700

    Use xz for initrd

--- scripts/post_image.sh
+++ scripts/post_image.sh
@@ -12,7 +12,7 @@ ROOTFS=/tmp/root
 mkdir -p ${ROOTFS}
 tar xJf ${IMAGES}/rootfs.tar.xz -C ${ROOTFS}
 cd ${ROOTFS}
-find | cpio -H newc -o | lzma -c > ${ISO}/boot/initrd
+find | cpio -H newc -o | xz -9 -C crc32 -c > ${ISO}/boot/initrd
 
 mkdir -p ${ISO}/boot/isolinux
 cp /usr/lib/syslinux/isolinux.bin ${ISO}/boot/isolinux/
