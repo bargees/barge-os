fb6778d6fb3467be1c59da66c9db4e4a20754321
A.I <ailis@paw.zone>
2015-08-24 17:40:25 -0700

    Add bash_cv_getcwd_malloc=yes for overlayfs

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,8 @@ set -e
 # Patch for GLIBC
 sed -e 's/utf8/utf-8/' -i support/dependencies/dependencies.sh
 
+# Add bash_cv_getcwd_malloc=yes for overlayfs
+sed -e '/^BASH_CONF_ENV += \\$/a \	bash_cv_getcwd_malloc=yes \\' -i package/bash/bash.mk
+
 make oldconfig
 make --quiet
