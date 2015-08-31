6ce1407af3aa465d5da9fbd312817ed840d1e7fb
A.I <ailis@paw.zone>
2015-08-31 14:49:40 -0700

    Add the basic startup scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,9 @@ sed -e 's/utf8/utf-8/' -i support/dependencies/dependencies.sh
 # Add bash_cv_getcwd_malloc=yes for overlayfs
 sed -e '/^BASH_CONF_ENV += \\$/a \	bash_cv_getcwd_malloc=yes \\' -i package/bash/bash.mk
 
+# Add the basics startup scripts
+cp -f ${OVERLAY}/etc/init.d/* package/initscripts/init.d/
+install -C -m 0755 package/initscripts/init.d/* ${OVERLAY}/etc/init.d/
+
 make oldconfig
 make --quiet
