240a409e82685392640fd9d0d3357e506e18603b
A.I <ailis@paw.zone>
2015-11-14 11:00:18 +0900

    buildroot:package/bash has been patched
    - http://git.buildroot.net/buildroot/commit/?id=0610de14901e5ad0f6c9b4db488477db47d2ff57

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,6 @@ set -e
 # Patch for GLIBC
 sed -e 's/utf8/utf-8/' -i support/dependencies/dependencies.sh
 
-# Add bash_cv_getcwd_malloc=yes for overlayfs
-sed -e '/^BASH_CONF_ENV += \\$/a \	bash_cv_getcwd_malloc=yes \\' -i package/bash/bash.mk
-
 # Add the basics startup scripts
 cp -f ${OVERLAY}/etc/init.d/* package/initscripts/init.d/
 install -C -m 0755 package/initscripts/init.d/* ${OVERLAY}/etc/init.d/
