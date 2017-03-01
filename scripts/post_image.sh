fa1de2a35a8031968be058fc909b69a99a685753
A.I <ailis@paw.zone>
2017-03-01 13:55:00 -0800

    Use the regular kernel image
    https://github.com/buildroot/buildroot/commit/5a09c1990808908416b6435e24a2ee10d7b3d7d3
    https://github.com/buildroot/buildroot/commit/3cca45ee6d516e9cd21f3bea9ba3d525c1805b47
    https://github.com/buildroot/buildroot/commit/43d08cb017750327524c7504cb004837462d6896

--- scripts/post_image.sh
+++ scripts/post_image.sh
@@ -15,11 +15,6 @@ find | cpio -H newc -o | gzip -9 -c > ${BINARIES_DIR}/initrd
 GENIMAGE_CFG="${SRC_DIR}/configs/genimage-rpi3.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
-# Mark the kernel as DT-enabled
-mkdir -p "${BINARIES_DIR}/kernel-marked"
-${HOST_DIR}/usr/bin/mkknlimg "${BINARIES_DIR}/zImage" \
-  "${BINARIES_DIR}/kernel-marked/zImage"
-
 rm -rf "${GENIMAGE_TMP}"
 
 ${HOST_DIR}/usr/bin/genimage                         \
