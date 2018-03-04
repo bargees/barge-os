4a430269ceef43f609572399648bfec932de2d48
A.I <ailis@paw.zone>
2018-03-04 13:49:37 -0800

    buildroot-2018.02
    - support/dependencies: unbreak check for UTF-8 locale

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Patch for GLIBC
-sed -e 's/utf8/utf-8/' -i support/dependencies/dependencies.sh
-
 # Add the basics startup scripts
 cp -f ${OVERLAY}/etc/init.d/* package/initscripts/init.d/
 install -C -m 0755 package/initscripts/init.d/* ${OVERLAY}/etc/init.d/
